46bc9150751e7704ea1a62f18d4fba5acd3cfd0a
tarasbobak <tarasbobak@gmail.com>
2015-05-27 01:27:17 +0300

    algorithm tasks rubocop

--- task1.rb
+++ task1.rb
@@ -1,16 +1,15 @@
-# Завдання 178 г
-# n - загальна кількість елементів послідовності натуральних чисел
+# Task 178 g
+# n - total quantity of elements of sequence of natural numbers
 n = 20
 res = 0
-# Замість числа 100 може бути вибрана інша верхня межа діапазону випадкових чисел
-array = Array.new(n){ |index| Random.rand(1..100)}
+# Any value can be set instead of 100 determining upper limit
+# of range of natural numbers
+array = Array.new(n) { Random.rand(1..100) }
 puts array.inspect
 k = 1
-while k<(n-1)
-    if array[k]<(array[k-1]+array[k+1])/2
-        res+=1
-    end
-    k+=1
+while k < (n - 1)
+  res += 1 if array[k] < (array[k - 1] + array[k + 1]) / 2
+  k += 1
 end
 puts 'Now lets see the result'
 puts res
--- task2.rb
+++ task2.rb
@@ -1,17 +1,17 @@
-# Завдання 178 д
-# n - загальна кількість елементів послідовності натуральних чисел
+# Task 178 d
+# n - total quantity of elements of sequence of natural numbers
 n = 20
 res = 0
-# Замість числа 100 може бути вибрана інша верхня межа діапазону випадкових чисел
-array = Array.new(n){ |index| Random.rand(1..100)}
+# Any value can be set instead of 100 determining upper limit
+# of range of natural numbers
+array = Array.new(n) { Random.rand(1..100) }
 puts array.inspect
 k = 0
-while k<n
-	# Використано k+1 оскільки 0 елемент масиву відповідає першому з n натуральних чисел і т.д. 
-    if 2**(k+1)<array[k] && array[k]<(1..k+1).inject(1, :*)
-        res+=1
-    end
-    k+=1
+while k < n
+  # k+1 is used, because 0 element of an array corresponds
+  # first item in the sequence of natural numbers and so on
+  res += 1 if 2**(k + 1) < array[k] && array[k] < (1..k + 1).inject(1, :*)
+  k += 1
 end
 puts 'Now lets see the result'
 puts res
